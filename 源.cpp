805fd03f32f7a17375b29c8cc95543357b596272
Vroomooo <117560253+Vroomooo@users.noreply.github.com>
2022-11-06 15:32:36 +0800

    Update 源.cpp

--- 源.cpp
+++ 源.cpp
@@ -1,27 +1,5 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<string.h>
-int main()
-{
-	FILE* fp,*fq;
-	char str[100];
-	int i,len,p=0,q=0;
-	fq = fopen("2.txt","w");
-	fp = fopen("1.txt","r");
-	fscanf(fp,"%s",str);
-	len = strlen(str);
-	for (i = 0; i<len; i++)
-	{
-		if (str[i]==" ")
-			p++;
-		if (str[i]==".")
-		{
-			p++;
-			q++;
-		}
-	}
-	fprintf(fq,"%d %d",p,q);
-	fclose(fp);
-	fclose(fq);
-	return 0;
-}
+#include<stdio.h> 
+ void main() 
+ { 
+ printf("Hello World！"); 
+ } 
